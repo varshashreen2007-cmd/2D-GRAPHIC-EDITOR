377fb4a10fa09d1df153f1d25ba90d50cbccfa93
varshashreen2007-cmd <varshashree.n2007@gmail.com>
2026-06-10 09:40:28 +0530

    Update main.c

--- main.c
+++ main.c
@@ -7,3 +7,22 @@ Void reserScreen()
 {
    int rowIdx,colIdx;
    for(rowIdx=0; rowIdx < MAX_ROWS; rowIdx++)
+   {
+      for(colIdx=0; colIdx <MAX-COLS; colIdx++)
+      {
+         screenBuffer[rowIdx][colIdx]='_';
+      }
+   }
+}
+void renderScreen()
+{
+   int r,c;
+   for(r=0;r< MAX_ROWS; r++)
+   {
+     for(c=0;c < MAX_COLS; c++)
+     {
+        printf("%c", screenBuffer[r][c]);
+     }
+     printf("\n");
+   }
+
